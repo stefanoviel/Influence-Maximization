87dd91ca6d244d0d723419638fa70351c5d37578
eliacunegatti <elia.cunegatti@studenti.unitn.it>
2021-09-30 18:07:28 +0200

    graphs

--- csv_to_txt.py
+++ csv_to_txt.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import os
 
-filename = "/Users/elia/Downloads/eco-mangwet/eco-mangwet.edges"
+filename = "/Users/elia/Downloads/USAir97/USAir97.mtx"
 name = (os.path.basename(filename))
 
 df = pd.read_csv(filename, sep=" ")
