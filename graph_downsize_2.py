175c3c5079179b2715948365e33aae1ac7976ac1
eliacunegatti <elia.cunegatti@studenti.unitn.it>
2021-10-04 13:52:57 +0200

    remove parser

---
+++ graph_downsize_2.py
@@ -0,0 +1,145 @@
+import networkx as nx
+import matplotlib.pylab as pl
+import numpy as np
+from community import community_louvain
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
+import networkx as nx
+from numpy.lib.function_base import append
+import load
+import pandas as pd
+import os
+#import igraph as ig
+#import leidenalg as la
+import os
+
+scale = 4
+resolution = 1
+
+filename = "graphs/Amazon0302.txt"
+name = (os.path.basename(filename))
+G = load.read_graph(filename)
+G = G.to_undirected()
+
+
+print(nx.info(G))
+
+
+position = nx.spring_layout(G)
+
+nx.draw(G, position,  edgecolors='black',node_color='white',arrowsize=1,node_size=20,linewidths=1, edge_color="#C0C0C0", width=0.5)
+plt.savefig("plot_graph/original-"+name+".png", dpi=1200)
+plt.cla()
+
+"""
+Resolution is a parameter for the Louvain community detection algorithm that affects the size of the 
+recovered clusters. Smaller resolutions recover smaller, and therefore a larger number of clusters, 
+and conversely, larger values recover clusters containing more data points.
+"""
+      
+partition = community_louvain.best_partition(G, resolution=resolution)
+
+##
+
+#G1 = ig.Graph.from_networkx(G)
+#partition = la.find_partition(G1, la.ModularityVertexPartition);
+#print(partition)
+#check = list(partition)
+# print(len(check))
+# check = check[:8]
+"""REDIFNE CHECK LIST HERE"""
+df = pd.DataFrame()
+df["nodes"] = list(partition.keys())
+df["comm"] = list(partition.values()) 
+df = df.groupby('comm')['nodes'].apply(list)
+df = df.reset_index(name='nodes')
+check = []
+for i in range(max(partition.values())+1):
+    check.append(df["nodes"].iloc[i])
+print(df)
+sum = 0
+check_ok = []
+
+for item in check:
+    sum = sum + len(item)
+
+    if len(item) > 2*scale:
+        check_ok.append(item)
+
+
+
+print("Total number of nodes after selection {0} \nCommunities before check {1} \nCommunities after check {2}".format(sum,len(check),len(check_ok)))
+check = check_ok
+
+
+
+list_edges = []
+for i in range(len(check)):
+    edge = 0
+    for k in range(0,len(check[i])):
+        for j in range(0,len(check[i])):
+            if check[i][k] != check[i][j]:
+                if G.has_edge(check[i][k],check[i][j]) == True:
+                    edge = edge + 1
+    
+    list_edges.append(edge/2)
+
+
+for i in range(len(check)):
+    print("Community {0} has {1} elements".format(i,len(check[i])))
+
+all_edges =  [[0 for x in range(len(check))] for y in range(len(check))] 
+for i in range(len(list_edges)):
+    nodes = len(check[i])
+    edges = list_edges[i]
+    print("Community {0} --> Edges = {1} , Nodes = {2}".format(i,edges,nodes))
+    all_edges[i][i] = float((2*edges)/(nodes*(nodes-1)))
+
+
+
+for i in range(len(check)):
+    edge = 0
+    for k in range(0,len(check[i])):
+        for j in range(0,len(check)):
+            if j!=i:
+                for item in check[j]:
+                    if G.has_edge(check[i][k],item) == True:
+                        edge = edge + 1    
+        
+                nodes = len(check[i]) + len(check[j])
+                all_edges[i][j] = float((edge)/(nodes*(nodes-1)))
+                all_edges[j][i] = float((edge)/(nodes*(nodes-1)))
+                if  all_edges[j][i] >1:
+                    all_edges[j][i] = 1
+                    all_edges[i][j] = 1
+                #print("For k {0} and j {1} number of nodes {2} and edges {3}".format(k,j,nodes,edge))
+
+
+
+
+sizes = []
+for item in check:
+    sizes.append(int(len(item)/scale))
+
+for i in range(len(sizes)):
+    print("Community {0} has {1} elements".format(i+1,sizes[i]))
+
+print(all_edges)
+
+g = nx.stochastic_block_model(sizes, all_edges, seed=0)
+print(nx.info(g))
+
+position = nx.spring_layout(g)
+
+    
+position = nx.spring_layout(g)
+
+nx.draw(g, position,  edgecolors='black',node_color='white',arrowsize=1,node_size=20,linewidths=1, edge_color="#C0C0C0", width=0.5)
+plt.savefig("plot_graph/SBM-"+name+"_"+ str(scale)+".png", dpi=1200)
+plt.cla()
+
+for u,v in g.edges():
+    f = "{0} {1}".format(u,v)
+    text.append(f) 
+with open("SBM-Graph/graph_"+str(name)+"_"+"scale_"+str(scale)+".txt", "w") as outfile:
+        outfile.write("\n".join(text))
