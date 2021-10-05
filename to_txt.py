2e77f12fd602f49b94ed7d40392d849c7ef97c4f
eliacunegatti <elia.cunegatti@studenti.unitn.it>
2021-10-05 14:22:43 +0200

    labeled graphs

---
+++ to_txt.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import os
+
+filename = "/Users/elia/Desktop/Influence-Maximization/graphs/soc-BlogCatalog-ASU/soc-BlogCatalog-ASU.edges"
+name = (os.path.basename(filename))
+name = name.replace(".edges","")
+
+df = pd.read_csv(filename, sep=",")
+print(df)
+print(df.index)
+n1 = df["node1"].to_list()
+n2 = df["node2"].to_list()
+
+text = []
+for i in range(len(n1)):
+
+    f = "{0} {1}".format(n1[i],n2[i])
+    text.append(f) 
+with open("graphs/"+ str(name)+".txt", "w") as outfile:
+        outfile.write("\n".join(text))
+        
