15b41a8774335d01b6d17f3ad22493d54c65e1bb
eliacunegatti <elia.cunegatti@studenti.unitn.it>
2021-10-22 16:52:08 +0200

    75

--- plot/prova.py
+++ plot/prova.py
@@ -5,7 +5,7 @@ import numpy as np
 import os
 sys.path.insert(0, '')
 from src.load import read_graph
-filename1 = "scale_results_csv/facebook_combined_5.csv"
+filename1 = "/Users/elia/Desktop/Influence-Maximization/scale_results_csv/facebook_combined_1.33.csv"
 df = pd.read_csv(filename1, sep=",")
 filename="graphs/facebook_combined.txt"
 g =read_graph(filename)
