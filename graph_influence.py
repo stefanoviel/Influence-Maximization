48631c70658c5882fab433c3cd5d0a63902c68b5
eliacunegatti <elia.cunegatti@studenti.unitn.it>
2021-10-30 11:15:44 +0200

    experiments

--- graph_influence.py
+++ graph_influence.py
@@ -85,7 +85,7 @@ if __name__ == '__main__':
                 mean.append(my_degree_function[item])
                 mean_degree.append(float(1/my_degree_function[item]))
 
-            p = 0.25
+            p = 0.4/scale
             args = {}
             args["p"] = p
             args["model"] = model
