3082ce7e516a5606bc9898a08dd819a606b1ecd1
eliacunegatti <elia.cunegatti@studenti.unitn.it>
2021-10-30 11:21:34 +0200

    experiments

--- graph_influence.py
+++ graph_influence.py
@@ -85,7 +85,7 @@ if __name__ == '__main__':
                 mean.append(my_degree_function[item])
                 mean_degree.append(float(1/my_degree_function[item]))
 
-            p = 0.4/scale
+            p = np.mean(mean_degree)
             args = {}
             args["p"] = p
             args["model"] = model
@@ -117,7 +117,7 @@ if __name__ == '__main__':
             '''
 
                     
-            no_simulations = 1
+            no_simulations = 50
             max_generations = 20
             #nodes' bound of seed sets
             #k=200
