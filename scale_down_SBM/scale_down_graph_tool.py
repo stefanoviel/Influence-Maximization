e03c19feb537031e2f6b7dce6cf931e0c78ab655
eliacunegatti <elia.cunegatti@studenti.unitn.it>
2021-11-17 14:30:39 +0100

    experiments

--- scale_down_SBM/scale_down_graph_tool.py
+++ scale_down_SBM/scale_down_graph_tool.py
@@ -13,7 +13,7 @@ import os
 
 
 from graph_tool.all import *
-scale = 2
+scale = 4
 resolution = 10
 
 filename = "graphs/facebook_combined.txt"
@@ -94,6 +94,7 @@ for i in range(len(list_edges)):
     edges = list_edges[i]
     print("Community {0} --> Edges = {1} , Nodes = {2}".format(i,edges,nodes))
     #all_edges[i][i] = float((2*edges)/(nodes*(nodes-1)))
+    avg = 
     all_edges[i][i] = edges
 
 n = (len(check) * len(check)) - len(check)
