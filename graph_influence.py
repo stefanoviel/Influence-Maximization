1e4183f57f66fe09443dcca86f4ece5d55f8c503
eliacunegatti <elia.cunegatti@studenti.unitn.it>
2021-11-18 11:41:04 +0100

    check experiments

--- graph_influence.py
+++ graph_influence.py
@@ -57,11 +57,11 @@ if __name__ == '__main__':
     #filenames = ["scale_graphs/graph_SBM_small_scale_5.txt","scale_graphs/graph_SBM_small_scale_4.txt","scale_graphs/graph_SBM_small_scale_3.txt","scale_graphs/graph_SBM_small_scale_2.txt","scale_graphs/graph_SBM_small_scale_1.5.txt","scale_graphs/graph_SBM_small_scale_1.33.txt","graphs/graph_SBM_small.txt"]
     #gt = ["comm_ground_truth/graph_SBM_small_5.csv","comm_ground_truth/graph_SBM_small_4.csv","comm_ground_truth/graph_SBM_small_3.csv","comm_ground_truth/graph_SBM_small_2.csv","comm_ground_truth/graph_SBM_small_1.5.csv","comm_ground_truth/graph_SBM_small_1.33.csv","comm_ground_truth/graph_SBM_small.csv"]
 
-    filenames = ["scale_graphs/facebook_combined.txt_example4.txt"]
+    filenames = ["comm_ground_truth/facebook_combined_1.csv","comm_ground_truth/facebook_combined.csv"]
 
-    gt = ["comm_ground_truth/facebook_combined_4.csv"]
-    scale_k=[4]
-    models = ["IC","IC_2","WC"]
+    gt = ["scale_graphs/facebook_combined.txt_example1.txt","graphs/facebook_combined.txt"]
+    scale_k=[1,1]
+    models = ["IC"]
     i = 0
     for item in filenames:
         file_gt = gt[i]
