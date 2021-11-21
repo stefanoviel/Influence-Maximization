121ad37dd9c079568919dbabe6862633dfd81bdc
eliacunegatti <elia.cunegatti@studenti.unitn.it>
2021-11-21 13:45:55 +0100

    fixing bug

--- graph_influence.py
+++ graph_influence.py
@@ -127,7 +127,7 @@ if __name__ == '__main__':
             '''
 
                     
-            no_simulations = 100
+            no_simulations = 1
             max_generations = 200
             #nodes' bound of seed sets
             #k=200
@@ -152,3 +152,4 @@ if __name__ == '__main__':
             
             exec_time = time.time() - start
             print(exec_time)
+            exit(0)
