2a04fd91751240f7631be27b6ec39ba0fb3d9adf
eliacunegatti <elia.cunegatti@studenti.unitn.it>
2021-11-21 12:33:17 +0100

    experiments degree + hypervolume + time

---
+++ color.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import networkx as nx
+from src.load import read_graph
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("graph_SBM_small-k100-p0.05-IC-degree.csv", sep=",")
+G = read_graph("graphs/graph_SBM_small.txt")
+
+color_map = []
+
+df = df.sort_values(by="n_nodes", ascending=True)
+nodes = df["nodes"]
+
+for item in nodes:
+    item = item.replace('[',"")
+    item = item.replace(']',"")
+    item = item.replace(',',"")
+    print(item)
+    n = item.split(" ")
+    x = [int(x) for x in n]
+    print(len(x))
+    color_map = ['red' if node in x else 'white' for node in G] 
+    position = nx.spring_layout(G)
+
+    nx.draw(G, position,  edgecolors='black',node_color=color_map,arrowsize=1,node_size=20,linewidths=1, edge_color="#C0C0C0", width=0.5)
+    plt.show()
+    plt.cla()
+    break
+
+
+
