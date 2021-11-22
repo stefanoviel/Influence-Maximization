5bc20dd4d65c6a13c689afa01295de56638a7ad5
eliacunegatti <elia.cunegatti@studenti.unitn.it>
2021-11-22 19:37:17 +0100

    experiments large fb

--- p.py
+++ p.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 #filenames = ["scale_graphs/graph_SBM_small_scale_5.txt","scale_graphs/graph_SBM_small_scale_4.txt","scale_graphs/graph_SBM_small_scale_3.txt","scale_graphs/graph_SBM_small_scale_2.txt","scale_graphs/graph_SBM_small_scale_1.5.txt","scale_graphs/graph_SBM_small_scale_1.33.txt","graphs/graph_SBM_small.txt"]
-filenames = ["scale_graphs/lastf_asia.txt_True-2.txt","lastf_asia.txt"]
+filenames = ["scale_graphs/large_facebook.txt_True-4.txt","large_facebook.txt"]
 #scale_graphs/graph_SBM_big.txt_example5.txt","graphs/graph_SBM_big.txt"]
 pp = []
 mm = []
@@ -66,11 +66,11 @@ for item in G1:
     mean_degree_1.append(float(1/my_degree_function[item]))
 print(max(mean_1), np.mean(mean_1), np.std(mean_1))
 print(den1/den)
-position = nx.spring_layout(G1)
+#position = nx.spring_layout(G1)
 
-nx.draw(G1, position,  edgecolors='black',node_color='white',arrowsize=1,node_size=20,linewidths=1, edge_color="#C0C0C0", width=0.5)
-plt.show()
-plt.cla()
+#nx.draw(G1, position,  edgecolors='black',node_color='white',arrowsize=1,node_size=20,linewidths=1, edge_color="#C0C0C0", width=0.5)
+#plt.show()
+#plt.cla()
 
 #position = nx.spring_layout(G)
 
