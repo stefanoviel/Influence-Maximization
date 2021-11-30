36a9f2fc0928961351946772ff8e536a59100f21
eliacunegatti <elia.cunegatti@studenti.unitn.it>
2021-11-30 12:05:56 +0100

    sbm experiments

--- scale_down_SBM/scale_down_graph_tool.py
+++ scale_down_SBM/scale_down_graph_tool.py
@@ -26,10 +26,10 @@ print(len(set(blocks)))
 state.draw(pos=g.vp.pos, output="football-sbm-fit.svg")
 exit(0)
 '''
-scale = 2
-resolution = 2
+scale = 4
+resolution = 1
 
-filename = "facebook_1912.txt"
+filename = "graphs/graph_SBM_small.txt"
 name = (os.path.basename(filename))
 G = read_graph(filename)
 G = G.to_undirected()
