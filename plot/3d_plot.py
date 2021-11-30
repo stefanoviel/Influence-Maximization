8f3f89561446ee9d7e3ddbf3df9ac7ad30d71464
eliacunegatti <elia.cunegatti@studenti.unitn.it>
2021-11-30 19:31:48 +0100

    experiments 2

--- plot/3d_plot.py
+++ plot/3d_plot.py
@@ -45,8 +45,8 @@ def plot(x2,y2,z2):
     plt.show()
 if __name__ == '__main__':
     
-    filename = "facebook_1684_TRUE-4-k5-p0-WC-degree.csv"
-    graph = "scale_graphs/facebook_1684.txt_TRUE-4.txt"
+    filename = "graph_SBM_small_TRUE-4-k5-p0.05-IC-degree.csv"
+    graph = "scale_graphs/graph_SBM_small.txt_TRUE-4.txt"
 
     G = read_graph(graph)
     N = G.number_of_nodes()
@@ -69,8 +69,8 @@ if __name__ == '__main__':
         x[i] = (x[i]/N) * 100
     
     plot(x,y,z)
-    filename = "facebook_1684_TRUE-2-k10-p0.05-IC-degree.csv"  
-    graph = "scale_graphs/facebook_1684.txt_TRUE-2.txt"
+    filename = "graph_SBM_small_TRUE-2-k10-p0.05-IC-degree.csv"  
+    graph = "scale_graphs/graph_SBM_small.txt_TRUE-2.txt"
 
     G = read_graph(graph)
     N = G.number_of_nodes()
@@ -149,11 +149,12 @@ if __name__ == '__main__':
     plt.scatter(z,x,color="green",label="25%")
     plt.scatter(z0,x0,color="blue", label="50%")
     #plt.scatter(z1,x1,color="orange",label="75%")
-    plt.scatter(z2,x2,color="red",label="100%")
+    #plt.scatter(z2,x2,color="red",label="100%")
     plt.title('Facebook WC model')
     plt.xlabel('% Influenced Nodes')
     plt.ylabel('% Nodes as seed set')
     plt.legend()
+    plt.xlim(0,100)
     plt.show()
 
 
