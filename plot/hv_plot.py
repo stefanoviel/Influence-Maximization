f2c38003b32b92fb10ad7fac0b410283f76507dc
eliacunegatti <elia.cunegatti@studenti.unitn.it>
2021-12-13 23:19:42 +0100

    new experiments

--- plot/hv_plot.py
+++ plot/hv_plot.py
@@ -5,11 +5,11 @@ sys.path.insert(0, '')
 from src.load import read_graph
 
 
-df = pd.read_csv("graph_SBM_small-k100-p0.05-IC-best_hv_hv_.csv",sep=",")
-df1 = pd.read_csv("facebook_combined_TRUE-2-k50-p0.05-IC-best_hv_hv_.csv",sep=",")
+df = pd.read_csv("graph_SBM_small-k100-p0-WC-best_hv_hv_.csv",sep=",")
+df1 = pd.read_csv("graph_SBM_small_TRUE-1.33-k75-p0-WC-best_hv_hv_.csv",sep=",")
 
-df2 = pd.read_csv("graph_SBM_small_TRUE-2-k50-p0.05-IC-best_hv_hv_.csv",sep=",")
-df4= pd.read_csv("graph_SBM_small_TRUE-4-k25-p0.05-IC-best_hv_hv_.csv",sep=",")
+df2 = pd.read_csv("graph_SBM_small_TRUE-2-k50-p0-WC-best_hv_hv_.csv",sep=",")
+df4= pd.read_csv("graph_SBM_small_TRUE-4-k25-p0-WC-best_hv_hv_.csv",sep=",")
 
 
 plt.plot(df.generation, df.hv, color="red", label="0riginal")
