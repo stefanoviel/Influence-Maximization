1c16588ca4049dc1930a382474147350f0165458
eliacunegatti <elia.cunegatti@studenti.unitn.it>
2022-01-04 10:24:56 +0100

    new time's calculation

--- p.py
+++ p.py
@@ -5,7 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-filenames = ["scale_graphs/deezerEU.txt_TRUE-8.0.txt","graphs/deezerEU.txt"]
+filenames = ["scale_graphs/fb_politician.txt_TRUE-4.0.txt","graphs/fb_politician.txt"]
 pp = []
 mm = []
 ma = []
