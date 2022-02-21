10ba1517b08767aedf8343594411a143bcac29cb
e.cunegatti@student.utwente.nl <s2878747@ctit007.ewi.utwente.nl>
2022-02-21 21:50:16 +0100

    results

--- map.py
+++ map.py
@@ -37,12 +37,12 @@ degree_measure = ['two-hop','page_rank', 'degree_centrality','katz_centrality', 
 MAP_RESULTS = {}
 for measure in degree_measure:
     MAP_RESULTS[measure] = []
-filename = "scale_graphs/fb_org_2.txt"
-scale_comm = "comm_ground_truth/fb_org_2.csv"
+filename = "scale_graphs/deezerEU_2.txt"
+scale_comm = "comm_ground_truth/deezerEU_2.csv"
 
 
-filename_original = "graphs/fb_org.txt"
-filename_original_comm = "comm_ground_truth/fb_org.csv"
+filename_original = "graphs/deezerEU.txt"
+filename_original_comm = "comm_ground_truth/deezerEU.csv"
 
 
 
@@ -57,11 +57,11 @@ scale_original = G1.number_of_nodes() / G.number_of_nodes()
 
 
 print('Scale Factor',scale_factor, scale_original)
-df_scale_results = pd.read_csv("experiments/fb_org_2-IC/run-1.csv",sep=",")
+df_scale_results = pd.read_csv("experiments/deezerEU_2-IC/run-1.csv",sep=",")
 df_scale_results = df_scale_results.sort_values(by="n_nodes", ascending=False)
 nodes = df_scale_results["nodes"].to_list()
 
-filename_original_results = "experiments/fb_org-IC/run-1.csv"
+filename_original_results = "experiments/deezerEU-IC/run-1.csv"
 
 def get_table(graph_name, comm_name, measure):
     G = read_graph(filename=graph_name)
@@ -243,7 +243,7 @@ for measure in degree_measure:
     from src.spread.monte_carlo import MonteCarlo_simulation
 
 
-    original_filename = "graphs/fb_org.txt"
+    original_filename = "graphs/deezerEU.txt"
     p = 0.05
     no_simulations = 100
     model = "IC"
@@ -283,7 +283,7 @@ for measure in degree_measure:
     df_mapping["n_nodes"] = nodes_
     df_mapping["influence"] = influence
     df_mapping["nodes"] = n_nodes
-    df_mapping.to_csv('fb_org_IC_2-'+str(measure)+'.csv', index=False)
+    df_mapping.to_csv('deezerEU_IC_2-'+str(measure)+'.csv', index=False)
 
 
 
@@ -350,5 +350,5 @@ df_final = pd.DataFrame()
 df_final["measure"] = measure
 df_final["Hyperarea"] = hv
 df_final["GD"] = gd
-df_final.to_csv('fb_org_IC_2_MAPPING.csv', index=False)
+df_final.to_csv('deezerEU_IC_2_MAPPING.csv', index=False)
 exit(0)
